e81c3f2ccd14b45da16a125af918dc175e67b365
Kyle Rudy <krudy@thesoftwareguild.com>
2018-05-18 15:14:38 -0500

    Removed extra square root code

--- src/main/java/com/sg/mersenneprime/MersennePrime.java
+++ src/main/java/com/sg/mersenneprime/MersennePrime.java
@@ -89,20 +89,6 @@ public class MersennePrime {
         }
         return true;
     }
-
-    public BigInteger sqrt(BigInteger n) {
-        BigInteger a = BigInteger.ONE;
-        BigInteger b = n.shiftRight(5).add(BigInteger.valueOf(8));
-        while (b.compareTo(a) >= 0) {
-            BigInteger mid = a.add(b).shiftRight(1);
-            if (mid.multiply(mid).compareTo(n) > 0) {
-                b = mid.subtract(BigInteger.ONE);
-            } else {
-                a = mid.add(BigInteger.ONE);
-            }
-        }
-        return a.subtract(BigInteger.ONE);
-    }
     
     public BigInteger squareRoot(BigInteger n) {
         
