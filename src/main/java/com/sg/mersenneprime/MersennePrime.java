0c7548108604586f8384bac6d47781f54e30e261
Kyle Rudy <krudy@thesoftwareguild.com>
2018-05-29 16:00:37 -0500

    Updated to use borrowed square root code and removed unneeded code that was causing a slow down

--- src/main/java/com/sg/mersenneprime/MersennePrime.java
+++ src/main/java/com/sg/mersenneprime/MersennePrime.java
@@ -31,14 +31,14 @@ public class MersennePrime {
 //        31:2147483647:16ms
 //        61:2305843009213693951:111887ms
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; /*RUN FOREVER*/; i++) {
             BigInteger value = BigInteger.valueOf(2).pow(i).subtract(BigInteger.ONE);
             long time = System.currentTimeMillis();
 
             if (isPrime(value)) {
                 System.out.println(i + ":" + value + ":" + (System.currentTimeMillis() - time) + "ms");
             } else {
-                //System.out.println("NO " + i + ":" + value);
+                //System.out.println("NO " + i + ":" + value + ":" + (System.currentTimeMillis() - time) + "ms");
             }
         }
     }
@@ -80,36 +80,36 @@ public class MersennePrime {
             return false;
         }
 
-        BigInteger max = squareRoot(value);
-        for (BigInteger bi = BigInteger.valueOf(3); bi.compareTo(max) < 0; bi = bi.add(two)) {
+        BigInteger max = sqrt(value);
+        for (BigInteger bi = BigInteger.valueOf(3); bi.compareTo(max) <= 0; bi = bi.add(two)) {
             if (value.mod(bi).equals(BigInteger.ZERO)) {
                 return false;
             }
-            max = value.divide(bi);
         }
         return true;
     }
-    
+
+    //Leaving here as an old example, using borrowed sqrt function below
     public BigInteger squareRoot(BigInteger n) {
-        
+
         //Get the power of 2 bitlength for the calculation
         int bitLength = n.bitLength();
-        for(int i = 0;i<100;i++) {
-            if(Math.pow(2, i) > bitLength) {
+        for (int i = 0; i < 100; i++) {
+            if (Math.pow(2, i) > bitLength) {
                 bitLength = (int) Math.pow(2, i);
                 break;
             }
         }
-        
+
         BigInteger res = BigInteger.ZERO;
-        BigInteger bit = BigInteger.ONE.shiftLeft(bitLength-2); 
-        
-        while(bit.compareTo(n) > 0) {
+        BigInteger bit = BigInteger.ONE.shiftLeft(bitLength - 2);
+
+        while (bit.compareTo(n) > 0) {
             bit = bit.shiftRight(2);
         }
-        
-        while(!bit.equals(BigInteger.ZERO)) {
-            if(n.compareTo(res.add(bit)) >= 0) {
+
+        while (!bit.equals(BigInteger.ZERO)) {
+            if (n.compareTo(res.add(bit)) >= 0) {
                 n = n.subtract(res.add(bit));
                 res = res.shiftRight(1).add(bit);
             } else {
@@ -118,5 +118,20 @@ public class MersennePrime {
             bit = bit.shiftRight(2);
         }
         return res;
+    }
+
+    //Taken from https://gist.github.com/JochemKuijpers/cd1ad9ec23d6d90959c549de5892d6cb
+    public BigInteger sqrt(BigInteger n) {
+        BigInteger a = BigInteger.ONE;
+        BigInteger b = n.shiftRight(5).add(BigInteger.valueOf(8));
+        while (b.compareTo(a) >= 0) {
+            BigInteger mid = a.add(b).shiftRight(1);
+            if (mid.multiply(mid).compareTo(n) > 0) {
+                b = mid.subtract(BigInteger.ONE);
+            } else {
+                a = mid.add(BigInteger.ONE);
+            }
+        }
+        return a.subtract(BigInteger.ONE);
     }
 }
